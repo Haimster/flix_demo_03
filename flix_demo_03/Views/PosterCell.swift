f637a3000b00a021dd6c3bd8d76d226668461aec
Haimei Yang <haimeiyang@wenges-mbp.dynamic.ucsd.edu>
2018-02-12 18:24:09 -0800

    submitting Assignment2

---
+++ flix_demo_03/Views/PosterCell.swift
@@ -0,0 +1,13 @@
+//
+//  PosterCell.swift
+//  flix_demo_03
+//
+//  Created by Haimei Yang on 2/6/18.
+//  Copyright © 2018 Haimei Yang. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+}
